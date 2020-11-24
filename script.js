db871c31e1bc2e2fcea73f392ad0101e375df446
tlk-hh <73819488+tlk-hh@users.noreply.github.com>
2020-11-24 13:15:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 $(function() {
+  
+  /** Daten für Timer*/
   function getCounterData(obj) {
     var hours = parseInt($('.number-hours', obj).text());
     var minutes = parseInt($('.number-minutes', obj).text());
     var seconds = parseInt($('.number-seconds', obj).text());
-    return seconds + (minutes * 60) + (hours * 3600);
+    
+    return seconds + (minutes * 600) + (hours * 3600);
   }
 
-  function setCounterData(s, obj) {
+  /**Startwerte */
+  function setCounterData(s, obj) {  
+
+    
     var hours = Math.floor((s % (60 * 60 * 24)) / (3600));
     var minutes = Math.floor((s % (60 * 60)) / 60);
     var seconds = Math.floor(s % 60);
-
+    
+    
     $('.number-hours', obj).html(hours);
     $('.number-minutes', obj).html(minutes);
-    $('.number-seconds', obj).html(seconds);
+    $('.number-seconds', obj).html(seconds); 
+    localStorage.setItem("number-hours", hours); 
+    localStorage.setItem("number-minutes", minutes);
+    localStorage.setItem("number-seconds",  seconds);
   }
 
-  var count = getCounterData($(".countdown-container"));
+  /** eigentlicher Countdown */
+  if (localStorage.getItem('counter') != null){
+    var count = localStorage.getItem("counter");
+  }else{
+    var count = 7200;
+  }
 
   var timer = setInterval(function() {
+    console.log(count);
+    localStorage.setItem("counter",  count);
     count--;
     if (count == 0) {
       $('.countdown-container').hide();
