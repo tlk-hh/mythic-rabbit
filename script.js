13d772cad3b3333afcfcb64482198cb4c85bed1f
tlk-hh <73819488+tlk-hh@users.noreply.github.com>
2020-12-14 15:16:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,9 +21,7 @@ $(function() {
     $('.number-hours', obj).html(hours);
     $('.number-minutes', obj).html(minutes);
     $('.number-seconds', obj).html(seconds); 
-    localStorage.setItem("number-hours", hours); 
-    localStorage.setItem("number-minutes", minutes);
-    localStorage.setItem("number-seconds",  seconds);
+   
   }
 
   /** eigentlicher Countdown */
@@ -36,11 +34,11 @@ $(function() {
   var timer = setInterval(function() {
     console.log(count);
     localStorage.setItem("counter",  count);
-    count--;
     if (count == 0) {
       $('.countdown-container').hide();
       return;
     }
+    count--;    
     setCounterData(count, $(".countdown-container"));
   }, 1000);
 });
